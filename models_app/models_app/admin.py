b2e192687206676e9ae93e4e03c8d9d73bb12fe3
Alexandria Baynes <alexandriadbaynes@gmail.com>
2021-05-27 11:15:13 -0500

    Updated Admin

--- models_app/models_app/admin.py
+++ models_app/models_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Student, School
+from .models import Student, School, Department, Grade, Certificate_type
 # Register your models here.
 
 admin.site.register(School)
 admin.site.register(Student)
+admin.site.register(Department)
+admin.site.register(Grade)
+admin.site.register(Certificate_type)
