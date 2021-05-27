b29537bf27c82a585908ba7cdabd9fb84ee0f8de
Alexandria Baynes <alexandriadbaynes@gmail.com>
2021-05-27 11:14:10 -0500

    Updated models and renamed a few

--- models_app/models_app/models.py
+++ models_app/models_app/models.py
@@ -1,36 +1,48 @@
 from django.db import models
 from datetime import datetime
-first_name = models.CharField(max_length=30)
-last_name = models.CharField(max_length=30)
+from django.contrib.auth.models import User
+
 
 # Create your models here
 
+
+        #ForeignKey (Faculty)
+class Faculty(models.Model):
+    faculty_name = models.CharField(max_length=40)
+
+
+    def __str__(self):
+        return self.faculty_name
+
+
+
+        #ForeignKey (Department)
+class Department(models.Model):
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    department_name = models.CharField(max_length=400)
+    
+
+
+    def __str__(self):
+        return self.department_name
+
+
+
+        #ForeingKey (School)
 class School(models.Model):
-    name = models.CharField(max_length=400)
+    school_name = models.CharField(max_length=400)
     address = models.CharField(max_length=400)
     postal_code = models.IntegerField()
 
 
     def __str__(self):
-        return self.name
-
-class Student(models.Model):
-    School = models.ForeignKey(School, on_delete=models.Cascade)
-    first_name +last_name = models.CharField(max_length=60)
-    Grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
-    year_of_graduation = models.IntegerField()
-    date_of_resumption = models.DateField(default=datetime.today)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    certificate_type = models.ForeignKey(Certificate_type, on_delete=models.Cascade)
+        return self.school_name
 
 
 
-    def __str__(self):
-        return self.first_name+last_name
-
-
+        #ForeignKey (Grade)
 class Grade(models.Model):
-    grade_type = models.IntegerField()
+    grade_type = models.CharField(max_length=40)
 
 
     def __str__(self):
@@ -38,10 +50,27 @@ class Grade(models.Model):
 
 
 
-class Department(models.Model):
-    department_name = models.CharField(max_length=400)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+        #ForeignKey (Certificate Type)
+class Certificate_type(models.Model):
+    certificate_type_name = models.CharField(max_length=200)
 
 
     def __str__(self):
-        return self.department_name
+        return self.certificate_type_name
+
+
+
+
+class Student(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    certificate_type = models.ForeignKey(Certificate_type, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=50)
+    year_of_graduation = models.IntegerField()
+    
+    
+
+    def __str__(self):
+        return self.full_name
+        
